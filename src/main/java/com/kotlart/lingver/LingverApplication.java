e99fb119a6f90e2528bbda98fa796f8ad32e3770
Artur Kotlyar <artur.kotlyar@gmail.com>
2019-02-09 14:57:22 +0200

    Update build

--- src/main/java/com/kotlart/lingver/LingverApplication.java
+++ src/main/java/com/kotlart/lingver/LingverApplication.java
@@ -2,20 +2,13 @@ package com.kotlart.lingver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class LingverApplication extends SpringBootServletInitializer implements WebMvcConfigurer {
+public class LingverApplication implements WebMvcConfigurer {
     public static void main(String[] args) {
         SpringApplication.run(LingverApplication.class, args);
-    }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(LingverApplication.class);
     }
 
     @Override
