1616bf78e1576635ad0b31749f163d8d6bc53881
Artur Kotlyar <artur.kotlyar@gmail.com>
2019-02-10 16:06:25 +0200

    Remove Word controller and repository

--- src/main/java/com/kotlart/lingver/respository/WordRepository.java
+++
@@ -1,7 +0,0 @@
-package com.kotlart.lingver.respository;
-
-import com.kotlart.lingver.model.Word;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WordRepository extends JpaRepository<Word, Long> {
-}
