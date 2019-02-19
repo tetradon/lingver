b930de98b60de78f9290622a49ca577ea4632020
Kotlyar, Artur <artur.kotlyar@sigma.software>
2019-02-19 13:18:49 +0200

    Cleanup protected route

--- lingver-ui/src/components/ProtectedRoute.js
+++ lingver-ui/src/components/ProtectedRoute.js
@@ -1,13 +1,10 @@
-import React, {Component} from "react";
+import React from "react";
 import {Redirect, Route} from "react-router-dom";
 
-export const ProtectedRoute = ({component: Component, ...rest}) => (
-    <Route {...rest} render={props => (
-        localStorage.getItem('user')
-            ? <Component {...props} />
-            : <Redirect to={{pathname: '/login', state: {from: props.location}}}/>
-    )}/>
-);
+export const ProtectedRoute = ({...props}) =>
+    localStorage.getItem('user')
+        ? <Route {...props}/>
+        : <Redirect to={{pathname: '/login', state: {from: props.location}}}/>;
 
 
 
