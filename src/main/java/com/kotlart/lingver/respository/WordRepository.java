7075d19bb90ed06d795e4d20fe6829e452a8bc5a
Kotlyar, Artur <artur.kotlyar@sigma.software>
2019-03-06 10:13:31 +0200

    Add word repository

---
+++ src/main/java/com/kotlart/lingver/respository/WordRepository.java
@@ -0,0 +1,8 @@
+package com.kotlart.lingver.respository;
+
+import com.kotlart.lingver.model.Word;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WordRepository extends JpaRepository<Word, Long> {
+    Word findByValue(String value);
+}
