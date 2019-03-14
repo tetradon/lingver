a198069526c82b2b67db23cc32a5e2baa00eb6e2
Kotlyar, Artur <artur.kotlyar@sigma.software>
2019-03-14 16:02:11 +0200

    Add search bar component

--- lingver-ui/src/components/TranslationTable.js
+++ lingver-ui/src/components/TranslationTable.js
@@ -14,9 +14,10 @@ import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
 import DeleteIcon from '@material-ui/icons/Delete';
-import {lighten} from '@material-ui/core/styles/colorManipulator';
+import {fade, lighten} from '@material-ui/core/styles/colorManipulator';
 import classNames from 'classnames';
 import ConfirmationDialog from "./ConfirmationDialog";
+import SearchBar from "./SearchBar";
 
 
 const styles = theme => ({
@@ -39,8 +40,50 @@ const styles = theme => ({
     title: {
         flex: '0 0 auto',
     },
+    search: {
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: fade(theme.palette.common.white, 0.15),
+        '&:hover': {
+            backgroundColor: fade(theme.palette.common.white, 0.25),
+        },
+        marginLeft: 0,
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            marginLeft: theme.spacing.unit,
+            width: 'auto',
+        },
+    },
+    searchIcon: {
+        width: theme.spacing.unit * 9,
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    inputRoot: {
+        color: 'inherit',
+        width: '100%',
+    },
+    inputInput: {
+        paddingTop: theme.spacing.unit,
+        paddingRight: theme.spacing.unit,
+        paddingBottom: theme.spacing.unit,
+        paddingLeft: theme.spacing.unit * 10,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            width: 120,
+            '&:focus': {
+                width: 200,
+            },
+        },
+    },
 
 });
+
 
 class TranslationTable extends Component {
     constructor(props) {
@@ -158,6 +201,7 @@ class TranslationTable extends Component {
                             )}
                         </div>
                         <div className={classes.spacer}/>
+                        <SearchBar/>
                         <div className={classes.actions}>
                             {this.state.selected.length > 0 ? (
                                 <Tooltip title="Delete">
@@ -166,7 +210,9 @@ class TranslationTable extends Component {
                                         <DeleteIcon/>
                                     </IconButton>
                                 </Tooltip>
-                            ) : null}
+                            ) : null
+
+                            }
                         </div>
                     </Toolbar>
                     <Table padding={"dense"}>
