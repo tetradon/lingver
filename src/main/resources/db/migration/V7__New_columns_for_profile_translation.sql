2a7070b6a591fef8c7e7b4acf60d3e7e78f79296
Kotlyar, Artur <artur.kotlyar@sigma.software>
2019-04-15 15:52:35 +0300

    Fix delta formatting

--- src/main/resources/db/migration/V7__New_columns_for_profile_translation.sql
+++ src/main/resources/db/migration/V7__New_columns_for_profile_translation.sql
@@ -1,6 +1,3 @@
-alter table profile_translation
-  add column last_repeat_date timestamp;
-alter table profile_translation
-  add column repeat_count int not null default 0;
-alter table profile_translation
-  add column success_repeat_count int not null default 0;
+alter table profile_translation add column last_repeat_date timestamp;
+alter table profile_translation add column repeat_count int not null default 0;
+alter table profile_translation add column success_repeat_count int not null default 0;
