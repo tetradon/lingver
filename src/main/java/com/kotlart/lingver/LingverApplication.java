512f640bbf1d329667ebc5396d226143608f1007
Artur Kotlyar <artur.kotlyar@gmail.com>
2019-05-12 12:08:23 +0300

    get rid of extra cors config

--- src/main/java/com/kotlart/lingver/LingverApplication.java
+++ src/main/java/com/kotlart/lingver/LingverApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
@@ -24,15 +23,6 @@ public class LingverApplication implements WebMvcConfigurer {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
-            }
-        };
-    }
 }
 
 
